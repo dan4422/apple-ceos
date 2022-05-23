de8cd1cb87bfc340bd5b26af1b9f10fac7274975
Daniel Lee <sdlee095@gmail.com>
2022-05-22 22:42:44 -0400

    completed exercise

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const express = require('express')
 const es6Renderer = require('express-es6-template-engine')
-const ceo = require('./data')
+const {ceo} = require('./data') // the curly brackets will get the same name exported module from the data file
+const data = require('./data') // gets all the exported module so you have to call the ceo array after to access it. Ex. data.ceo
+// const ceo = data.ceo
 
 const hostname = 'localhost'
 const port = 3000
@@ -21,8 +23,48 @@ app.use(express.static('./public'))
 
 // routes
 app.get('/', (req,res) => {
+  const name = req.query.name
+  if (name) {
+    const ceoDetail = ceo.find(c => c.name.toLowerCase() == name.toLowerCase())
+    res.render('ceo-details', {
+      partials,
+      locals: {
+        ceoDetail,
+        title: `${ceoDetail.name}'s details page`
+      }
+    })
+    return
+  }
+
   res.render('home', {
     partials,
+    locals: {
+      ceo,
+      title: "Apple CEO Homepage!",
+    }
+  })
+}
+)
+
+app.get('/CEOs', (req,res) => {
+  res.render('ceo-list', {
+    partials,
+    locals: {
+      ceo,
+      title: "CEO-List",
+    }
+  })
+})
+
+app.get('/ceos/:slug', (req,res) => {
+  const {slug} = req.params
+  const ceoDetail = ceo.find(c => c.slug == slug)
+  res.render('ceo-details', {
+    partials,
+    locals: {
+      ceoDetail,
+      title: `${ceoDetail.name}'s details page`
+    }
   })
 })
 
